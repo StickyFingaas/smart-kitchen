7b622127dc8305ba7805efb37d595a1087b176b9
abramovicaleksa <abramovicaleksa@gmail.com>
2021-03-29 18:23:40 +0200

    Item init.

---
+++ src/main/java/me/fit/smartkitchen/model/Item.java
@@ -0,0 +1,19 @@
+package me.fit.smartkitchen.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+
+public class Item {
+	
+	@Id
+	@SequenceGenerator(name = "itemSequence", sequenceName = "item_id_sequence", allocationSize = 1, initialValue = 1)
+	@GeneratedValue(generator = "itemSequence")
+	private Long id;
+	private String name;
+	private Integer price;
+	
+}
