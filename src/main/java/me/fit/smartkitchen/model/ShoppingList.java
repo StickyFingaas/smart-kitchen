34ef241e85787f9247a955ed9549239557b4ad65
abramovicaleksa <abramovicaleksa@gmail.com>
2021-04-04 14:23:55 +0200

    ShoppingList + ShoppingListItem insert.

--- src/main/java/me/fit/smartkitchen/model/ShoppingList.java
+++ src/main/java/me/fit/smartkitchen/model/ShoppingList.java
@@ -3,13 +3,93 @@ package me.fit.smartkitchen.model;
 import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
 
 @Entity
 
 public class ShoppingList {
 
+	@Id
+	@SequenceGenerator(name = "itemSequence", sequenceName = "item_id_sequence", allocationSize = 1, initialValue = 1)
+	@GeneratedValue(generator = "itemSequence")
 	private Long id;
 	private String name;
+	@OneToMany
+	private Set<ShoppingListItem> items;
+	@OneToOne
+	private User user;
 	
-	private Set<Item> items;
+	public ShoppingList(String name, Set<ShoppingListItem> items) {
+		super();
+		this.name = name;
+		this.items = items;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<ShoppingListItem> getItems() {
+		return items;
+	}
+
+	public void setItems(Set<ShoppingListItem> items) {
+		this.items = items;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ShoppingList other = (ShoppingList) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "ShoppingList [id=" + id + ", name=" + name + ", items=" + items + ", user=" + user + "]";
+	}
+	
+	
 }
