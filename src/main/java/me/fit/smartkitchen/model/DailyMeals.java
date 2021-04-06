86c63287190df2ba47fd4ec340bdcaf534f740ac
LIDIJA V <LIDIJA V@DESKTOP-DU0IR02>
2021-04-06 11:01:15 +0200

    Edit (+id)

--- src/main/java/me/fit/smartkitchen/model/DailyMeals.java
+++ src/main/java/me/fit/smartkitchen/model/DailyMeals.java
@@ -4,14 +4,17 @@ import java.sql.Time;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 
 @Entity
 public class DailyMeals {
 	
-//	@SequenceGenerator(name = "itemSequence", sequenceName = "item_id_sequence", allocationSize = 1, initialValue = 1)
-//	@GeneratedValue(generator = "itemSequence")
+	@Id
+	@SequenceGenerator(name = "itemSequence", sequenceName = "item_id_sequence", allocationSize = 1, initialValue = 1)
+	@GeneratedValue(generator = "itemSequence")
+	private Long id;
 	
 	@ManyToOne
 	private DailyMealsRecipe breakfast;
@@ -26,15 +29,23 @@ public class DailyMeals {
 	private Time reminderL;
 	private Time reminderD;
 	
-	public DailyMeals(DailyMealsRecipe breakfast, DailyMealsRecipe lunch, DailyMealsRecipe dinner, Time reminderB,
-			Time reminderL, Time reminderD) {
+	public DailyMeals(Long id, DailyMealsRecipe breakfast, DailyMealsRecipe lunch, DailyMealsRecipe dinner,
+			Time reminderB, Time reminderL, Time reminderD) {
 		super();
+		this.id = id;
 		this.breakfast = breakfast;
 		this.lunch = lunch;
 		this.dinner = dinner;
 		this.reminderB = reminderB;
 		this.reminderL = reminderL;
 		this.reminderD = reminderD;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
 	}
 	public DailyMealsRecipe getBreakfast() {
 		return breakfast;
@@ -72,68 +83,36 @@ public class DailyMeals {
 	public void setReminderD(Time reminderD) {
 		this.reminderD = reminderD;
 	}
-	
-//	@Override
-//	public int hashCode() {
-//		final int prime = 31;
-//		int result = 1;
-//		result = prime * result + ((breakfast == null) ? 0 : breakfast.hashCode());
-//		result = prime * result + ((dinner == null) ? 0 : dinner.hashCode());
-//		result = prime * result + ((lunch == null) ? 0 : lunch.hashCode());
-//		result = prime * result + ((reminderB == null) ? 0 : reminderB.hashCode());
-//		result = prime * result + ((reminderD == null) ? 0 : reminderD.hashCode());
-//		result = prime * result + ((reminderL == null) ? 0 : reminderL.hashCode());
-//		return result;
-//	}
-	
-//	@Override
-//	public boolean equals(Object obj) {
-//		if (this == obj)
-//			return true;
-//		if (obj == null)
-//			return false;
-//		if (getClass() != obj.getClass())
-//			return false;
-//		DailyMeals other = (DailyMeals) obj;
-//		if (breakfast == null) {
-//			if (other.breakfast != null)
-//				return false;
-//		} else if (!breakfast.equals(other.breakfast))
-//			return false;
-//		if (dinner == null) {
-//			if (other.dinner != null)
-//				return false;
-//		} else if (!dinner.equals(other.dinner))
-//			return false;
-//		if (lunch == null) {
-//			if (other.lunch != null)
-//				return false;
-//		} else if (!lunch.equals(other.lunch))
-//			return false;
-//		if (reminderB == null) {
-//			if (other.reminderB != null)
-//				return false;
-//		} else if (!reminderB.equals(other.reminderB))
-//			return false;
-//		if (reminderD == null) {
-//			if (other.reminderD != null)
-//				return false;
-//		} else if (!reminderD.equals(other.reminderD))
-//			return false;
-//		if (reminderL == null) {
-//			if (other.reminderL != null)
-//				return false;
-//		} else if (!reminderL.equals(other.reminderL))
-//			return false;
-//		return true;
-//	}
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DailyMeals other = (DailyMeals) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 	@Override
 	public String toString() {
-		return "DailyMeals [breakfast=" + breakfast + ", lunch=" + lunch + ", dinner=" + dinner + ", reminderB="
-				+ reminderB + ", reminderL=" + reminderL + ", reminderD=" + reminderD + "]";
+		return "DailyMeals [id=" + id + ", breakfast=" + breakfast + ", lunch=" + lunch + ", dinner=" + dinner
+				+ ", reminderB=" + reminderB + ", reminderL=" + reminderL + ", reminderD=" + reminderD + "]";
 	}
-	
-	
 
 }
